bf7dcbc6e5c03f715c15a4b2d9a838a2d65cfbe9
Alexander Kedrik <alkedr@yandex-team.ru>
2020-06-19 23:59:38 +0300

    shopt -s histappend

--- bashrc.d/50-ethernal-history.sh
+++ bashrc.d/50-ethernal-history.sh
@@ -9,5 +9,6 @@ export HISTTIMEFORMAT="[%F %T] "
 # http://superuser.com/questions/575479/bash-history-truncated-to-500-lines-on-each-login
 export HISTFILE=~/.bash_eternal_history
 # Force prompt to write history after every command.
-# http://superuser.com/questions/20900/bash-history-loss
+# https://askubuntu.com/questions/67283/is-it-possible-to-make-writing-to-bash-history-immediate
+shopt -s histappend
 PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
